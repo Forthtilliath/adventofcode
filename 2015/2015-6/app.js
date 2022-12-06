d4ef058af1c52fc28ba9e13f226e86045f7baff3
Forth <33458877+Forthtilliath@users.noreply.github.com>
2022-12-06 18:54:42 +0100

    2015-7

--- 2015/2015-6/app.js
+++ 2015/2015-6/app.js
@@ -10,8 +10,8 @@ import { fetchLines, sum } from "../../lib/forth.js";
 function part1(lines) {
   const ACTIONS = {
     toggle: (...args) => setLights(...args, (n) => Number(!n)),
-    on: (...args) => setLights(...args, () => 1),
-    off: (...args) => setLights(...args, () => 0),
+    "turn on": (...args) => setLights(...args, () => 1),
+    "turn off": (...args) => setLights(...args, () => 0),
   };
   return doActions(lines, ACTIONS);
 }
@@ -19,8 +19,8 @@ function part1(lines) {
 function part2(lines) {
   const ACTIONS = {
     toggle: (...args) => setLights(...args, (n) => n + 2),
-    on: (...args) => setLights(...args, (n) => n + 1),
-    off: (...args) => setLights(...args, (n) => Math.max(n - 1, 0)),
+    "turn on": (...args) => setLights(...args, (n) => n + 1),
+    "turn off": (...args) => setLights(...args, (n) => Math.max(n - 1, 0)),
   };
   return doActions(lines, ACTIONS);
 }
@@ -30,19 +30,14 @@ function doActions(lines, ACTIONS) {
   const lights = Array.from({ length }, (_) => Array(length).fill(0));
 
   lines.forEach((line) => {
-    // console.log(line);
-    const procs = line.split(" ");
-    const action = procs.length === 5 ? procs.at(1) : procs.at(0);
-    ACTIONS[action](lights, procs.at(-3), procs.at(-1));
-    // console.log(count(lights));
+    const regex = /([\D]+) ([\d]+),([\d]+) through ([\d]+),([\d]+)/;
+    const [_, action, ...slots] = line.match(regex);
+    ACTIONS[action](lights, ...slots);
   });
   return count(lights);
 }
 
-function setLights(arr, from, to, setter) {
-  const [fromX, fromY] = from.split(",").map(Number);
-  const [toX, toY] = to.split(",").map(Number);
-
+function setLights(arr, fromX, fromY, toX, toY, setter) {
   for (let i = fromX; i <= toX; i++) {
     for (let j = fromY; j <= toY; j++) {
       arr[j][i] = setter(arr[j][i]);
